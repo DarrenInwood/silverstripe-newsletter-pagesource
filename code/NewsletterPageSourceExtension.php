e2ca21b905e480aff49213a5e3ae072b57e50b7f
Darren Inwood <darren@stripetheweb.com>
2015-06-05 14:25:24 +1200

    Correct to remove build errors on SS3.1

--- code/NewsletterPageSourceExtension.php
+++ code/NewsletterPageSourceExtension.php
@@ -4,16 +4,17 @@
  *
  * @package silverstripe-newsletter-pagesource
  */
-class NewsletterPageSourceExtension extends DataObjectDecorator {
+class NewsletterPageSourceExtension extends DataExtension {
 
-	public function extraStatics() {
-		return array(
-			'db'      => array('ContentSource' => 'Enum("content, page", "content")'),
-			'has_one' => array('SourcePage' => 'SiteTree')
-		);
-	}
+	private static $db = array(
+		'ContentSource' => 'Enum("content, page", "content")'
+	);
 
-	public function updateCMSFields(FieldSet $fields) {
+	private static $has_one = array(
+		'SourcePage' => 'SiteTree',
+	);
+
+	public function updateCMSFields(FieldList $fields) {
 		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
 		Requirements::javascript(THIRDPARTY_DIR . '/jquery-livequery/jquery.livequery.js');
 		Requirements::javascript('newsletter-pagesource/javascript/NewsletterAdmin.js');
